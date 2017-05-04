464fa4739d060c1e46fdde955af793dede0f6b2d
Shubham Patel <Shubham.patel23@yahoo.com>
2017-05-03 20:25:06 -0400

    Added fields to data collection
    -Created alternate risk value that compared the given risk value to
    their passing (or lack thereof)
    -Parsed original data for zipcode to create locality data
    -Added multiple different visualizations to Tableau Workbook

--- src/Food.java
+++ src/Food.java
@@ -1,15 +1,14 @@
-import java.util.ArrayList;
 
 public class Food {
 
 	public static void main(String[] args) {
 		
-		String file1 = "C:\\Users\\kschool\\Downloads\\Food_Inspections.csv";
+		String file1 = "C:\\Users\\Shubham\\Downloads\\Food_Inspections.csv";
 		
 		/** My machine uses only one core, so I am using one runner **/
-		FoodRunner r1 = new FoodRunner(file1);
-		r1.run();
-
+		FoodRunner runner = new FoodRunner(file1);
+		runner.run();
+		
 	}
 
 }
